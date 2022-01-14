47689a43e3f951e2fc360656a0c2905ad9d97618
TheNeoStormZ <73660500+TheNeoStormZ@users.noreply.github.com>
2022-01-14 16:53:45 +0100

    T10 completado

--- src/main/java/org/springframework/samples/petclinic/recoveryroom/RecoveryRoomController.java
+++ src/main/java/org/springframework/samples/petclinic/recoveryroom/RecoveryRoomController.java
@@ -3,9 +3,13 @@ package org.springframework.samples.petclinic.recoveryroom;
 import java.util.List;
 import java.util.Map;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class RecoveryRoomController {
@@ -28,6 +32,19 @@ public class RecoveryRoomController {
 		model.put("types", list);
 		
 		return VIEWS_ROOMS_CREATE_OR_UPDATE_FORM;
+	}
+	
+	@PostMapping(value = "/recoveryroom/create")
+	public String processCreationForm(@Valid RecoveryRoom room, BindingResult result) {
+		if (result.hasErrors()) {
+			return VIEWS_ROOMS_CREATE_OR_UPDATE_FORM;
+		}
+		else {
+			//creating owner, user and authorities
+			this.roomService.save(room);
+			
+			return "welcome";
+		}
 	}
 	
 	
