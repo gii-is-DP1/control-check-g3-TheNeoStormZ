ac6a20c0d3eaf95509bc65c066bb6219cbb561a2
TheNeoStormZ <73660500+TheNeoStormZ@users.noreply.github.com>
2022-01-14 16:35:12 +0100

    T7 completado

--- src/main/java/org/springframework/samples/petclinic/recoveryroom/RecoveryRoomTypeFormatter.java
+++ src/main/java/org/springframework/samples/petclinic/recoveryroom/RecoveryRoomTypeFormatter.java
@@ -3,20 +3,35 @@ package org.springframework.samples.petclinic.recoveryroom;
 import java.text.ParseException;
 import java.util.Locale;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.Formatter;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RecoveryRoomTypeFormatter implements Formatter<RecoveryRoomType>{
 
+	private RecoveryRoomService rs;
+	
+	@Autowired
+    public RecoveryRoomTypeFormatter(RecoveryRoomService rs) {
+		this.rs = rs;
+	}
+ 
+	
     @Override
     public String print(RecoveryRoomType object, Locale locale) {
-        return null;
+    	return object.getName();
     }
 
-    @Override
+
+	@Override
     public RecoveryRoomType parse(String text, Locale locale) throws ParseException {
-        return null;
+    	RecoveryRoomType rrt = rs.getRecoveryRoomType(text);
+         
+         if(rrt == null) {
+         	throw new ParseException(text , 0);
+         }
+ 		return rrt;
     }
     
 }
